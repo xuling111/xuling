cb9626a29e50ae72078860f1b3d745dec2b37faa
xuling111 <1336809132@qq.com>
2019-03-12 12:33:42 +0800

    第一次作业，徐令

---
+++ 999/2,17/work/2.16.cpp
@@ -0,0 +1,13 @@
+#include <iostream>allows program to perform input and output
+using namespace std;
+
+int main()
+{
+    int x,y;
+    cin >>x>>y;
+    cout<<x+y<<endl;
+    cout<<x*y<<endl;
+    cout<<x-y<<endl;
+    cout<<x/y<<endl;
+    return 0;
+}
---
+++ 999/2,17/work/2.20.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int x;
+    cin>>x;
+    cout<<2*x<<endl;
+    cout<<3.14159*2*x<<endl;
+    cout<<3.14159*x*x<<endl;
+    return 0;
+}
